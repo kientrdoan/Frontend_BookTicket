85c9dcf929d76bd061aba0a4bff12c777f800f6e
trkien <dr.kientrdoan@gmail.com>
2025-07-24 22:40:33 +0700

    update UI login

--- src/components/RSlick/MultipleRowSlick.jsx
+++ src/components/RSlick/MultipleRowSlick.jsx
@@ -4,7 +4,10 @@ import styleSlick from "./MultipleRowSlick.module.css";
 import Film from "../film/Film";
 import Film_Flip from "../Film/Film_Flip";
 import { useDispatch, useSelector } from "react-redux";
-import { SET_FILM_DANG_CHIEU, SET_FILM_SAP_CHIEU } from "../../redux/actions/types/QuanLyPhimType";
+import {
+  SET_FILM_DANG_CHIEU,
+  SET_FILM_SAP_CHIEU,
+} from "../../redux/actions/types/QuanLyPhimType";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -29,17 +32,18 @@ function SamplePrevArrow(props) {
 }
 
 const MultipleRowSlick = (props) => {
-  const dispatch = useDispatch()
-  const {dangChieu,sapChieu} = useSelector(state => state.QuanLyPhimReducer);
-  
-  let activeClassDC = dangChieu===true ? 'active_Film' : 'none_active_Film';
-  let activeClassSC = sapChieu === true ? 'active_Film' : 'none_active_Film';
+  const dispatch = useDispatch();
+  const { dangChieu, sapChieu } = useSelector(
+    (state) => state.QuanLyPhimReducer
+  );
 
+  let activeClassDC = dangChieu === true ? "active_Film" : "none_active_Film";
+  let activeClassSC = sapChieu === true ? "active_Film" : "none_active_Film";
 
   const renderFilms = () => {
     return props.arrFilm.slice(0, 12).map((item, index) => {
       return (
-        <div className="mt-2" key={index}>
+        <div className='mt-2' key={index}>
           <Film_Flip phim={item}></Film_Flip>
         </div>
       );
@@ -62,25 +66,28 @@ const MultipleRowSlick = (props) => {
 
   return (
     <div>
-      <button
-        className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`}
-        onClick={() => {
-          const action = { type: SET_FILM_DANG_CHIEU };
-          dispatch(action);
-        }}
-      >
-        PHIM ĐANG CHIẾU
-      </button>
+      <div className="flex justify-center">
+        <button
+          className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`}
+          onClick={() => {
+            const action = { type: SET_FILM_DANG_CHIEU };
+            dispatch(action);
+          }}
+        >
+          PHIM ĐANG CHIẾU
+        </button>
 
-      <button
-        className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`}
-        onClick={() => {
-          const action = { type: SET_FILM_SAP_CHIEU };
-          dispatch(action);
-        }}
-      >
-        PHIM SẮP CHIẾU
-      </button>
+        <button
+          className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`}
+          onClick={() => {
+            const action = { type: SET_FILM_SAP_CHIEU };
+            dispatch(action);
+          }}
+        >
+          PHIM SẮP CHIẾU
+        </button>
+      </div>
+
       <Slider {...settings}>{renderFilms()}</Slider>
     </div>
   );
